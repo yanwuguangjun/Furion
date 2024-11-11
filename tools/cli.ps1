8716e76826c1d0303f4b89204bcb64a933e3d5be
百小僧 <monksoul@outlook.com>
2024-11-12 00:56:04 +0800

    😊 更新 `v4` 版本工具版本

--- tools/cli.ps1
+++ tools/cli.ps1
@@ -96,7 +96,7 @@ function AddXmlCommentsToProperties($content, $commentsDictionary) {
     return $modifiedContent
 }
 
-$FurTools = "Furion Tools v4.9.5.17";
+$FurTools = "Furion Tools v4.9.5.18";
 
 # 输出信息
 $copyright = @"
