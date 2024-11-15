390a78437e27e9e9c6db9510334193abe5be05e3
百小僧 <monksoul@outlook.com>
2024-11-15 16:56:53 +0800

    😊 发布 `v4.9.5.22` 版本，修复粘土对象序列化问题

--- tools/cli.ps1
+++ tools/cli.ps1
@@ -96,7 +96,7 @@ function AddXmlCommentsToProperties($content, $commentsDictionary) {
     return $modifiedContent
 }
 
-$FurTools = "Furion Tools v4.9.5.21";
+$FurTools = "Furion Tools v4.9.5.22";
 
 # 输出信息
 $copyright = @"
