4cc25e69f4104fe395ac24051d4499d949ce9929
百小僧 <monksoul@outlook.com>
2024-11-22 11:57:09 +0800

    😊 发布 `v4.9.5.25` 版本，修复授权异常和 `UseUnifyResultStatusCodes()` 中间件冲突问题

--- tools/cli.ps1
+++ tools/cli.ps1
@@ -96,7 +96,7 @@ function AddXmlCommentsToProperties($content, $commentsDictionary) {
     return $modifiedContent
 }
 
-$FurTools = "Furion Tools v4.9.5.24";
+$FurTools = "Furion Tools v4.9.5.25";
 
 # 输出信息
 $copyright = @"
